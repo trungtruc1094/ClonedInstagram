f115c3098aabb8136f527db7ecda76cfb0d8dc0d
Trung Truc <1259035@itec.hcmus.edu.vn>
2018-07-25 22:49:05 +1200

    commit Feed
    commit Feed mockup

--- app/src/main/java/com/example/trung/clonedinstagram/MainActivity.java
+++ app/src/main/java/com/example/trung/clonedinstagram/MainActivity.java
@@ -3,27 +3,53 @@ package com.example.trung.clonedinstagram;
 import android.content.ClipData;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar) RelativeLayout toolbar;
+    @BindView(R.id.rvFeed) RecyclerView rvFeed;
+//    RecyclerView rvFeed;
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
+    private FeedAdapter feedAdapter;
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.main_menu, menu);
 //        MenuItem inboxMenuItem = menu.findItem(R.id.action_inbox);
 //        inboxMenuItem.setActionView(R.layout.menu_item_view);
-        return true;
-    }
+//        return true;
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        rvFeed = (RecyclerView) findViewById(R.id.rvFeed);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_menu_white);
+//        setupToolbar();
+        setupFeed();
+    }
+
+    /*private void setupToolbar() {
+        setSupportActionBar(toolbar);
+    }*/
+
+    private void setupFeed() {
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        rvFeed.setLayoutManager(linearLayoutManager);
+
+        feedAdapter = new FeedAdapter(this);
+        rvFeed.setAdapter(feedAdapter);
     }
 }
